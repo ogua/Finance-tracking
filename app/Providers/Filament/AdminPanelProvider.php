6dc4e7e60de42c259a37816c015bb068e5abc600
ogua ahmed <ogua.ahmed18@gmail.com>
2024-04-18 02:08:26 +0000

    updated

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,42 +17,49 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Leandrocfe\FilamentApexCharts\FilamentApexChartsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
+        ->default()
+        ->plugins([
+            FilamentApexChartsPlugin::make()
+            ])
             ->id('admin')
             ->path('admin')
             ->login()
+            ->sidebarFullyCollapsibleOnDesktop()
             ->colors([
                 'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
-    }
-}
+                ])
+                ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+                ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+                ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
+                ->pages([
+                    Pages\Dashboard::class,
+                    ])
+                    ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+                    ->widgets([
+                        Widgets\AccountWidget::class,
+                        //Widgets\FilamentInfoWidget::class,
+                        ])
+                        ->middleware([
+                            EncryptCookies::class,
+                            AddQueuedCookiesToResponse::class,
+                            StartSession::class,
+                            AuthenticateSession::class,
+                            ShareErrorsFromSession::class,
+                            VerifyCsrfToken::class,
+                            SubstituteBindings::class,
+                            DisableBladeIconComponents::class,
+                            DispatchServingFilamentEvent::class,
+                            ])
+                            ->authMiddleware([
+                                Authenticate::class,
+                            ]);
+                        }
+                    }
+                    
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('address')->nullable();
+            $table->string('public_key')->nullable();
+            $table->string('private_key')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
